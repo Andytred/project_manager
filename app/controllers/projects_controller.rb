3def9562ffe747b5141a1d2ecd8eb6649c55ed73
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-04-16 12:06:43 +0200

    fixing the delete method in project contoller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,15 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_project, only: %i[ show edit update destroy ]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-  # GET /projects or /projects.json
+  # GET /projects
+  # GET /projects.json
   def index
     @projects = current_user.projects
   end
 
-  # GET /projects/1 or /projects/1.json
+  # GET /projects/1
+  # GET /projects/1.json
   def show
     @task = @project.tasks.build
   end
@@ -21,39 +23,42 @@ class ProjectsController < ApplicationController
   def edit
   end
 
-  # POST /projects or /projects.json
+  # POST /projects
+  # POST /projects.json
   def create
     @project = current_user.projects.build(project_params)
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to @project, notice: "Project was successfully created." }
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
         format.json { render :show, status: :created, location: @project }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /projects/1 or /projects/1.json
+  # PATCH/PUT /projects/1
+  # PATCH/PUT /projects/1.json
   def update
     respond_to do |format|
       if @project.update(project_params)
-        format.html { redirect_to @project, notice: "Project was successfully updated." }
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
         format.json { render :show, status: :ok, location: @project }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /projects/1 or /projects/1.json
+  # DELETE /projects/1
+  # DELETE /projects/1.json
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
+      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
