0c8d2990bbe2f638a8d9bcdfd663783f383f86bb
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-04-23 12:07:36 +0200

    implementing test for Task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validate test" do
+    it 'ensures title presence'do
+      task = Task.new(title: 'title').save
+  end
+   it 'ensures description presence'do
+      task = Task.new(description: 'title').save
+  end
+    it 'ensures boolean presence'do
+      task = Task.new(is_done: false).save
+  end
+    
+end
 end
