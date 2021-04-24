8aed678883cc1f7fb8e30616cbca92db66435a87
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-04-24 13:06:28 +0200

    styling of navbar

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  context "validate test" do
+    it 'ensures title presence'do
+      task = Task.new(title: 'title').save
+    end
+    it 'ensures description presence'do
+        task = Task.new(description: 'title').save
+    end
+      it 'ensures boolean presence'do
+        task = Task.new(is_done: false).save
+    end
+  end
+end
